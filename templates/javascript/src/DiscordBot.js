36211797c96714660f0a47103b06847dc857d039
Jeroen Claassens <jeroen.claassens@live.nl>
2019-06-03 22:20:13 +0200

    More comments in template

--- templates/javascript/src/DiscordBot.js
+++ templates/javascript/src/DiscordBot.js
@@ -13,9 +13,8 @@ const { oneLine } = require('common-tags');
 const moment = require('moment');
 
 class DiscordBot {
-
   /**
-   * Constructor for the Discord Bot. Sets the token to the class as well as 
+   * Constructor for the Discord Bot. Sets the token to the class as well as
    * created the client with the given prefix and owner ID. You can put
    * a lot more configuration here, view the Discord.JS-Commando docs
    * on CommandoClient here: {@link https://discord.js.org/#/docs/commando/djs-v11/class/CommandoClient}
@@ -31,7 +30,7 @@ class DiscordBot {
 
   /**
    * Initializes the constructed client
-   * This method has all the logic of 
+   * This method has all the logic of
    *   - event handling
    *   - Settings provider initialization
    *   - Registry initialization
@@ -40,25 +39,37 @@ class DiscordBot {
    */
   init () {
     this.client
+      // Logs errors
       .on('error', (err) => log.error(err))
+      // Logs when the bot is ready
       .on('ready', () => log.info(this.readyMessage()))
+      // Logs warnings
       .on('warn', (warning) => log.warn(warning));
 
+    // Ensures the bot doesn't crash when it runs into an unhandled rejection, an otherwise process crashing NodeJS exception
     process.on('unhandledRejection', (reason, promise) => log.error(reason));
 
+    // Set the client provider to an sqlite database
     this.client.setProvider(
       sqlite.open(path.join(__dirname, 'data', 'databases', 'settings.sqlite3')).then(db => new SQLiteProvider(db))
     ).catch(console.error);
 
+    // Initialize the bot registry
     this.client.registry
+      // Register the types given by Commando
       .registerDefaultTypes()
+      // Register the default command groups given by Commando
       .registerDefaultGroups()
+      // Register the default commands given by Commando (help, eval, ping etc.)
+      .registerDefaultCommands()
+      // Register our own command groups
       .registerGroups([
         ['info', 'Informational commands']
       ])
-      .registerDefaultCommands()
+      // Register our own commands
       .registerCommandsIn(path.join(__dirname, 'commands'));
 
+    // Login the bot
     return this.client.login(this.token);
   }
 
